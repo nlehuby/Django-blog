5576c284c372f374f6ab2248fd9d7f88b15ddd37
Noémie <session1@Pitou.(none)>
2012-01-10 22:21:11 +0100

    ajout de l'interface d'admin

--- urls.py
+++ urls.py
@@ -1,16 +1,21 @@
 from django.conf.urls.defaults import *
 
+from django.contrib import admin
+
+
+
 # Uncomment the next two lines to enable the admin:
+
 # from django.contrib import admin
-# admin.autodiscover()
+
+admin.autodiscover()
+
+
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^djangoblog/', include('djangoblog.foo.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+     (r'^admin/(.*)', admin.site.root),
 
-    # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
 )
+
+
